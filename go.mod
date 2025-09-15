1b8c1b43cdf41565475734369d7b3ba91fb35e67
Vinicius Simm <vinao.simm@gmail.com>
2025-09-14 23:00:32 -0300

    :tada: basic functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ViniciusSimm/url-shortener
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+// In-memory store for testing
+var urlStore = make(map[string]string)
+
+// All characters that can be used to create the new URL
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+var sizeLetterBytes int = len(letterBytes)
+
+func init() {
+	
+	// Run before the main function to make sure the generated URLs are different
+	rand.Seed(time.Now().UnixNano())
+}
+
+func generateRandomString(n int) string {
+
+	b := make([]byte, n)
+	for i := range b {
+
+		// Place a random character into the byte slice
+		b[i] = letterBytes[rand.Intn(sizeLetterBytes)]
+
+	}
+
+	// Return the byte slice as a string
+	return string(b)
+
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request, amountOfCharactersToUse int) {
+
+	if r.Method != http.MethodPost {
+
+		http.Error(w, "Endpoint only accepts POST requests", http.StatusMethodNotAllowed)
+		return
+
+	}
+
+	originalURL := r.FormValue("url")
+
+	if originalURL == "" {
+
+		http.Error(w, "The URL must not be empty", http.StatusBadRequest)
+	
+	}
+
+	// Generate a random string
+	randomString := generateRandomString(amountOfCharactersToUse)
+
+	// Store in the map
+	urlStore[randomString] = originalURL
+
+	shortURL := fmt.Sprintf("http://localhost:8080/%s", randomString)
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "Short URL: %s\n", shortURL)
+
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Drop the leading "/"
+	shortURL := r.URL.Path[1:]
+
+	originalURL, found := urlStore[shortURL]
+
+	if !found {
+
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+
+	}
+
+	if originalURL == "" {
+		http.Error(w, "The URL must not be empty", http.StatusBadRequest)
+	}
+
+	http.Redirect(w, r, originalURL, http.StatusFound)
+
+}
+
+func main() {
+
+	fmt.Println("Type the number of characters in the shortened variable")
+	amountOfCharactersToUse := 8
+	fmt.Scan(&amountOfCharactersToUse)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/shorten" {
+			shortenHandler(w, r, amountOfCharactersToUse)
+		} else {
+			// Any other path is treated as shortened URL
+			redirectHandler(w, r)
+		}
+	})
+
+	fmt.Println("Starting server on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Printf("Initiation failed: %s\n", err)
+	}
+}
